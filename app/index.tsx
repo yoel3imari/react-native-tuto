68faa2d0a9a4e5640fd0f9864c6e265cc944c9b4
yoel3imari <youssef.elaimari@gmail.com>
2024-11-17 14:29:06 +0100

    setup

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import "@/global.css"
+
+
+function index() {
+  return (
+    <View>
+      <Text className="text-rose-500 text-2xl">index</Text>
+    </View>
+  )
+}
+
+export default index
--- src/components/header/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Text } from "react-native";
-
-function Index () {
-  return <Text>Header</Text>
-}
-
-export default Index;
